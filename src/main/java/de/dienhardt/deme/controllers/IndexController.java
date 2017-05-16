5d6283bdec7c72baa3ed5c7ad15822ea018f14fb
Harald Pusch <harald.pusch@adesso.de>
2017-05-16 18:37:00 +0200

    Kurzes PoC für dynamische Teaser in eigenem Service

--- src/main/java/de/dienhardt/deme/controllers/IndexController.java
+++ src/main/java/de/dienhardt/deme/controllers/IndexController.java
@@ -1,18 +1,20 @@
 package de.dienhardt.deme.controllers;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.thymeleaf.TemplateEngine;
+import org.springframework.web.context.WebApplicationContext;
+import org.thymeleaf.context.IContext;
 import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.StandardTemplateModeHandlers;
+import org.thymeleaf.spring4.context.SpringWebContext;
 
 import de.dienhardt.deme.service.JsonContentService;
 import lombok.NonNull;
@@ -23,53 +25,51 @@ public class IndexController {
 	private final JsonContentService contentService;
 
 	@Autowired
-	private ApplicationContext applicationContext;
+	private WebApplicationContext applicationContext;
+
+	@Autowired
+	private SpringTemplateEngine templateEngine;
 
 	@Autowired
 	public IndexController(@NonNull JsonContentService contentService) {
 		this.contentService = contentService;
 	}
 
-	@Bean
-	public SpringResourceTemplateResolver templateResolver() {
-		// SpringResourceTemplateResolver automatically integrates with Spring's
-		// own resource resolution infrastructure, which is highly recommended.
-		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-		templateResolver.setApplicationContext(this.applicationContext);
-		templateResolver.setPrefix("/WEB-INF/templates/");
-		templateResolver.setSuffix(".html");
-		// HTML is the default value, added here for the sake of clarity.
-		templateResolver.setTemplateMode(StandardTemplateModeHandlers.HTML5.getTemplateModeName());
-		// Template cache is true by default. Set to false if you want
-		// templates to be automatically updated when modified.
-		templateResolver.setCacheable(true);
-		return templateResolver;
-	}
-
-	@Bean
-	public SpringTemplateEngine templateEngine() {
-		// SpringTemplateEngine automatically applies SpringStandardDialect and
-		// enables Spring's own MessageSource message resolution mechanisms.
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver());
-		return templateEngine;
-	}
-
-	@Bean
-	public ThymeleafViewResolver viewResolver() {
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine());
-		return viewResolver;
-	}
-
 	@RequestMapping("/")
-	String index(ModelMap model, TemplateEngine templateEngine) {
+	String index(ModelMap model, HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		Map<String, Object> page = contentService.getPage();
-		// System.out.println(templateEngine.process("index", ???));
+
+		Map<String, Object> pageMap = (Map<String, Object>) page.get("page");
+		List<Map<String, Object>> sections = (List<Map<String, Object>>) pageMap.get("sections");
+		for (Map<String, Object> section : sections) {
+			Object sectionTemplateObject = section.get("sectionTemplate");
+			if (sectionTemplateObject != null && sectionTemplateObject instanceof String) {
+				String sectionTemplate = (String) sectionTemplateObject;
+				if ("dynamic".equals(sectionTemplate)) {
+					SpringWebContext ctx = new SpringWebContext(req, resp, applicationContext.getServletContext(), Locale.GERMAN, new ModelMap(), applicationContext);
+					handleDynamicSection(section, page, ctx);
+				}
+			}
+		}
 
 		model.addAllAttributes(page);
 		model.addAttribute("message", "Hello Freaks!");
+
+		System.out.println(page);
+
 		return "index";
+	}
+
+	private void handleDynamicSection(Map<String, Object> section, Map<String, Object> page, IContext ctx) {
+		// templateEngine.process("templates/exhibitorTeaser", ctx);
+
+		Object typeObject = section.get("type");
+		if (typeObject != null && typeObject instanceof String) {
+			String type = (String) typeObject;
+			if ("exhibitorTeaser".equals(type)) {
+				section.put("markup", "<h3>Exhibitor Teaser component here. Tag: " + section.get("tag") + "</h3>");
+			}
+		}
 	}
 
 }
